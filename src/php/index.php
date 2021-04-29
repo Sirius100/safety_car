233d3528b0f3a0e465f2f4df103667998638c9b9
ESA <esaul-00@mail.ru>
2021-04-29 17:31:10 +0300

    init project

---
+++ src/php/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ru-RU">
+  <head>
+    <link rel="stylesheet" href="src/css/style.css">
+    <!--link(rel='preconnect' href='https://fonts.gstatic.com')-->
+    <!--link(href='https://fonts.googleapis.com/css2?family=Slabo+27px&display=swap' rel='stylesheet')-->
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="title" content="Обзор новых возможностей">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit-no">
+    <meta name="robots" content="noindex">
+    <meta name="Author" content="ФИО автора">
+    <meta name="Copyright" content="кому принадлежат авторские права">
+    <meta name="Address" content="адрес где создавался контент">
+    <title>Bizcompany</title>
+  </head>
+  <body>
+    <!-- 1  -  header-->
+    <div class="container-fuild"></div>
+    <!-- 2  - strahovanie-->
+    <div class="container-fuild"></div>
+    <!-- 3  - findstrahovoy-->
+    <div class="container-fuild"></div>
+    <!-- 4  - imgcar-->
+    <div class="container-fuild"></div>
+    <!-- 5  - beststrahovih_1-->
+    <div class="container-fuild"></div>
+    <!-- 6  - beststrahovih_2-->
+    <div class="container-fuild"></div>
+    <!-- 7  - quest-->
+    <div class="container-fuild"></div>
+    <!-- 8  - safety-->
+    <div class="container-fuild"></div>
+    <!-- 9  - tocontact-->
+    <div class="container-fuild"></div>
+    <!-- 10 -  footer-->
+    <div class="container-fuild"></div>
+    <script src="gulp/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="gulp/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ src/pug/templates/link.pug
@@ -0,0 +1,2 @@
+<link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"> 
+<link href="https://fonts.googleapis.com/css?family=Raleway:600&display=swap" rel="stylesheet"> 
